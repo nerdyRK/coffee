a235a78942a8a6996d1a039511b19f6648a8b274
Ravi Kumar <rav7179@gmail.com>
2023-10-22 11:49:29 +0530

    fixed sec 1 content

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 let menubtn=document.querySelector('.burger')
 let menu=document.querySelector('.nav-links')
+let container=document.querySelector('.container-1')
+
+// let imgs=["images/bg_2.jpg","images/bg_1.jpg","images/bg_2.jpg"]
+
+// let currentIndex = 0;
+
+// setInterval(() => {
+//     container.classList.toggle(`show-img${currentIndex++}`)
+// }, 5000)
 
 menubtn.addEventListener("click",()=>{
     menu.classList.toggle("menu-show")
